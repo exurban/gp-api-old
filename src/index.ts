b84dbb3dc78d9b55370a521c2c9d23721fca879b
Brad Gibbs <bradgibbs@mac.com>
2020-11-05 18:35:40 -0600

    e

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ const getOptions = async () => {
     extra: {
       ssl: true,
     },
-    entities: ["./entities/**/*.js"],
+    entities: ["./entities/*.js"],
     namingStrategy: new SnakeNamingStrategy(),
   };
   if (process.env.DATABASE_URL) {
