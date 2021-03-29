adaf294cd266f013a0934e71d07471f879efdafa
Brad Gibbs <bradgibbs@mac.com>
2021-03-29 18:54:18 -0500

    sharing

--- src/resolvers/PhotoResolver.ts
+++ src/resolvers/PhotoResolver.ts
@@ -888,7 +888,9 @@ export default class PhotoResolver {
       };
     }
 
-    console.log(`updating photo ${photo.id} with input: ${input}`);
+    console.log(
+      `updating photo ${photo.id} with input: ${JSON.stringify(input, null, 2)}`
+    );
 
     photo.title = input.title || photo.title;
     photo.description = input.description || photo.description;
